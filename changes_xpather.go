fa1869a221216eff04177b87e0e119d58f35eab9
Vadim Markovtsev <vadim@sourced.tech>
2018-03-09 15:48:10 +0100

    Increase the default number of UAST workers
    Signed-off-by: Vadim Markovtsev <vadim@sourced.tech>

--- changes_xpather.go
+++ changes_xpather.go
@@ -3,12 +3,12 @@ package hercules
 import (
 	"log"
 
+	"bytes"
 	"github.com/minio/highwayhash"
 	"gopkg.in/bblfsh/client-go.v2/tools"
 	"gopkg.in/bblfsh/sdk.v1/uast"
 	"gopkg.in/src-d/go-git.v4/plumbing"
 	"io"
-	"bytes"
 )
 
 // ChangesXPather extracts changed UAST nodes from files changed in the current commit.
