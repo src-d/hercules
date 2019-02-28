b2dc585c81122c6f96d0961287c3a108ebe7038d
Vadim Markovtsev <vadim@sourced.tech>
2019-02-28 17:41:50 +0100

    Port --devs and --file-history to LinesStats
    Signed-off-by: Vadim Markovtsev <vadim@sourced.tech>

--- leaves/file_history.go
+++ leaves/file_history.go
@@ -3,6 +3,7 @@ package leaves
 import (
 	"fmt"
 	"io"
+	"log"
 	"sort"
 	"strings"
 
@@ -14,65 +15,77 @@ import (
 	"gopkg.in/src-d/hercules.v8/internal/core"
 	"gopkg.in/src-d/hercules.v8/internal/pb"
 	items "gopkg.in/src-d/hercules.v8/internal/plumbing"
+	"gopkg.in/src-d/hercules.v8/internal/plumbing/identity"
 )
 
-// FileHistory contains the intermediate state which is mutated by Consume(). It should implement
+// FileHistoryAnalysis contains the intermediate state which is mutated by Consume(). It should implement
 // LeafPipelineItem.
-type FileHistory struct {
+type FileHistoryAnalysis struct {
 	core.NoopMerger
 	core.OneShotMergeProcessor
-	files map[string][]plumbing.Hash
+	files      map[string]*FileHistory
+	lastCommit *object.Commit
 }
 
 // FileHistoryResult is returned by Finalize() and represents the analysis result.
 type FileHistoryResult struct {
-	Files map[string][]plumbing.Hash
+	Files map[string]FileHistory
+}
+
+// FileHistory is the gathered stats about a particular file.
+type FileHistory struct {
+	// Hashes is the list of commit hashes which changed this file.
+	Hashes []plumbing.Hash
+	// People is the mapping from developers to the number of lines they altered.
+	People map[int]items.LineStats
 }
 
 // Name of this PipelineItem. Uniquely identifies the type, used for mapping keys, etc.
-func (history *FileHistory) Name() string {
-	return "FileHistory"
+func (history *FileHistoryAnalysis) Name() string {
+	return "FileHistoryAnalysis"
 }
 
 // Provides returns the list of names of entities which are produced by this PipelineItem.
 // Each produced entity will be inserted into `deps` of dependent Consume()-s according
 // to this list. Also used by core.Registry to build the global map of providers.
-func (history *FileHistory) Provides() []string {
+func (history *FileHistoryAnalysis) Provides() []string {
 	return []string{}
 }
 
 // Requires returns the list of names of entities which are needed by this PipelineItem.
 // Each requested entity will be inserted into `deps` of Consume(). In turn, those
 // entities are Provides() upstream.
-func (history *FileHistory) Requires() []string {
-	arr := [...]string{items.DependencyTreeChanges}
+func (history *FileHistoryAnalysis) Requires() []string {
+	arr := [...]string{items.DependencyTreeChanges, items.DependencyLineStats, identity.DependencyAuthor}
 	return arr[:]
 }
 
 // ListConfigurationOptions returns the list of changeable public properties of this PipelineItem.
-func (history *FileHistory) ListConfigurationOptions() []core.ConfigurationOption {
+func (history *FileHistoryAnalysis) ListConfigurationOptions() []core.ConfigurationOption {
 	return []core.ConfigurationOption{}
 }
 
 // Flag for the command line switch which enables this analysis.
-func (history *FileHistory) Flag() string {
+func (history *FileHistoryAnalysis) Flag() string {
 	return "file-history"
 }
 
 // Description returns the text which explains what the analysis is doing.
-func (history *FileHistory) Description() string {
-	return "Each file path is mapped to the list of commits which involve that file."
+func (history *FileHistoryAnalysis) Description() string {
+	return "Each file path is mapped to the list of commits which touch that file and the mapping " +
+		"from involved developers to the corresponding line statistics: how many lines were added, " +
+		"removed and changed throughout the whole history."
 }
 
 // Configure sets the properties previously published by ListConfigurationOptions().
-func (history *FileHistory) Configure(facts map[string]interface{}) error {
+func (history *FileHistoryAnalysis) Configure(facts map[string]interface{}) error {
 	return nil
 }
 
 // Initialize resets the temporary caches and prepares this PipelineItem for a series of Consume()
 // calls. The repository which is going to be analysed is supplied as an argument.
-func (history *FileHistory) Initialize(repository *git.Repository) error {
-	history.files = map[string][]plumbing.Hash{}
+func (history *FileHistoryAnalysis) Initialize(repository *git.Repository) error {
+	history.files = map[string]*FileHistory{}
 	history.OneShotMergeProcessor.Initialize()
 	return nil
 }
@@ -82,46 +95,91 @@ func (history *FileHistory) Initialize(repository *git.Repository) error {
 // Additionally, DependencyCommit is always present there and represents the analysed *object.Commit.
 // This function returns the mapping with analysis results. The keys must be the same as
 // in Provides(). If there was an error, nil is returned.
-func (history *FileHistory) Consume(deps map[string]interface{}) (map[string]interface{}, error) {
-	if !history.ShouldConsumeCommit(deps) {
+func (history *FileHistoryAnalysis) Consume(deps map[string]interface{}) (map[string]interface{}, error) {
+	if deps[core.DependencyIsMerge].(bool) {
+		// we ignore merge commits
+		// TODO(vmarkovtsev): handle them better
 		return nil, nil
 	}
-	commit := deps[core.DependencyCommit].(*object.Commit).Hash
+	history.lastCommit = deps[core.DependencyCommit].(*object.Commit)
+	commit := history.lastCommit.Hash
 	changes := deps[items.DependencyTreeChanges].(object.Changes)
 	for _, change := range changes {
 		action, _ := change.Action()
+		var fh *FileHistory
+		if action != merkletrie.Delete {
+			fh = history.files[change.To.Name]
+		} else {
+			fh = history.files[change.From.Name]
+		}
+		if fh == nil {
+			fh = &FileHistory{}
+			history.files[change.To.Name] = fh
+		}
 		switch action {
 		case merkletrie.Insert:
-			hashes := make([]plumbing.Hash, 1)
-			hashes[0] = commit
-			history.files[change.To.Name] = hashes
+			fh.Hashes = []plumbing.Hash{commit}
 		case merkletrie.Delete:
-			delete(history.files, change.From.Name)
+			fh.Hashes = append(fh.Hashes, commit)
 		case merkletrie.Modify:
-			hashes := history.files[change.From.Name]
+			hashes := history.files[change.From.Name].Hashes
 			if change.From.Name != change.To.Name {
 				delete(history.files, change.From.Name)
 			}
 			hashes = append(hashes, commit)
-			history.files[change.To.Name] = hashes
+			fh.Hashes = hashes
+		}
+	}
+	lineStats := deps[items.DependencyLineStats].(map[object.ChangeEntry]items.LineStats)
+	author := deps[identity.DependencyAuthor].(int)
+	for changeEntry, stats := range lineStats {
+		file := history.files[changeEntry.Name]
+		if file == nil {
+			file = &FileHistory{}
+			history.files[changeEntry.Name] = file
+		}
+		people := file.People
+		if people == nil {
+			people = map[int]items.LineStats{}
+			file.People = people
+		}
+		oldStats := people[author]
+		people[author] = items.LineStats{
+			Added:   oldStats.Added + stats.Added,
+			Removed: oldStats.Removed + stats.Removed,
+			Changed: oldStats.Changed + stats.Changed,
 		}
 	}
 	return nil, nil
 }
 
 // Finalize returns the result of the analysis. Further Consume() calls are not expected.
-func (history *FileHistory) Finalize() interface{} {
-	return FileHistoryResult{Files: history.files}
+func (history *FileHistoryAnalysis) Finalize() interface{} {
+	files := map[string]FileHistory{}
+	fileIter, err := history.lastCommit.Files()
+	if err != nil {
+		log.Panicf("Failed to iterate files of %s", history.lastCommit.Hash.String())
+	}
+	err = fileIter.ForEach(func(file *object.File) error {
+		if fh := history.files[file.Name]; fh != nil {
+			files[file.Name] = *fh
+		}
+		return nil
+	})
+	if err != nil {
+		log.Panicf("Failed to iterate files of %s", history.lastCommit.Hash.String())
+	}
+	return FileHistoryResult{Files: files}
 }
 
 // Fork clones this PipelineItem.
-func (history *FileHistory) Fork(n int) []core.PipelineItem {
+func (history *FileHistoryAnalysis) Fork(n int) []core.PipelineItem {
 	return core.ForkSamePipelineItem(history, n)
 }
 
 // Serialize converts the analysis result as returned by Finalize() to text or bytes.
 // The text format is YAML and the bytes format is Protocol Buffers.
-func (history *FileHistory) Serialize(result interface{}, binary bool, writer io.Writer) error {
+func (history *FileHistoryAnalysis) Serialize(result interface{}, binary bool, writer io.Writer) error {
 	historyResult := result.(FileHistoryResult)
 	if binary {
 		return history.serializeBinary(&historyResult, writer)
@@ -130,7 +188,7 @@ func (history *FileHistory) Serialize(result interface{}, binary bool, writer io
 	return nil
 }
 
-func (history *FileHistory) serializeText(result *FileHistoryResult, writer io.Writer) {
+func (history *FileHistoryAnalysis) serializeText(result *FileHistoryResult, writer io.Writer) {
 	keys := make([]string, len(result.Files))
 	i := 0
 	for key := range result.Files {
@@ -139,27 +197,44 @@ func (history *FileHistory) serializeText(result *FileHistoryResult, writer io.W
 	}
 	sort.Strings(keys)
 	for _, key := range keys {
-		hashes := result.Files[key]
+		fmt.Fprintf(writer, "  - %s:\n", key)
+		file := result.Files[key]
+		hashes := file.Hashes
 		strhashes := make([]string, len(hashes))
 		for i, hash := range hashes {
 			strhashes[i] = "\"" + hash.String() + "\""
 		}
-		fmt.Fprintf(writer, "  - %s: [%s]\n", key, strings.Join(strhashes, ","))
+		sort.Strings(strhashes)
+		fmt.Fprintf(writer, "    commits: [%s]\n", strings.Join(strhashes, ","))
+		strpeople := make([]string, 0, len(file.People))
+		for key, val := range file.People {
+			strpeople = append(strpeople, fmt.Sprintf("%d:[%d,%d,%d]", key, val.Added, val.Removed, val.Changed))
+		}
+		sort.Strings(strpeople)
+		fmt.Fprintf(writer, "    people: {%s}\n", strings.Join(strpeople, ","))
 	}
 }
 
-func (history *FileHistory) serializeBinary(result *FileHistoryResult, writer io.Writer) error {
+func (history *FileHistoryAnalysis) serializeBinary(result *FileHistoryResult, writer io.Writer) error {
 	message := pb.FileHistoryResultMessage{
 		Files: map[string]*pb.FileHistory{},
 	}
 	for key, vals := range result.Files {
-		hashes := &pb.FileHistory{
-			Commits: make([]string, len(vals)),
+		fh := &pb.FileHistory{
+			Commits:            make([]string, len(vals.Hashes)),
+			ChangesByDeveloper: map[int32]*pb.LineStats{},
 		}
-		for i, hash := range vals {
-			hashes.Commits[i] = hash.String()
+		for i, hash := range vals.Hashes {
+			fh.Commits[i] = hash.String()
 		}
-		message.Files[key] = hashes
+		for key, val := range vals.People {
+			fh.ChangesByDeveloper[int32(key)] = &pb.LineStats{
+				Added:   int32(val.Added),
+				Removed: int32(val.Removed),
+				Changed: int32(val.Changed),
+			}
+		}
+		message.Files[key] = fh
 	}
 	serialized, err := proto.Marshal(&message)
 	if err != nil {
@@ -170,5 +245,5 @@ func (history *FileHistory) serializeBinary(result *FileHistoryResult, writer io
 }
 
 func init() {
-	core.Registry.Register(&FileHistory{})
+	core.Registry.Register(&FileHistoryAnalysis{})
 }
