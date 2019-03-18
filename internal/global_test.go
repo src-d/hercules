d4ef2dd50cc54430153218bcb639fe37248a4b7f
Robert Lin <robertlin1@gmail.com>
2019-03-18 14:28:38 -0700

    Update pipeline serialization test
    Signed-off-by: Robert Lin <robertlin1@gmail.com>

--- internal/global_test.go
+++ internal/global_test.go
@@ -28,18 +28,18 @@ func TestPipelineSerialize(t *testing.T) {
 	dot := string(bdot)
 	assert.Equal(t, `digraph Hercules {
   "6 BlobCache" -> "7 [blob_cache]"
-  "0 TicksSinceStart" -> "3 [tick]"
   "9 FileDiff" -> "11 [file_diff]"
   "15 FileDiffRefiner" -> "16 Burndown"
-  "1 IdentityDetector" -> "4 [author]"
+  "0 IdentityDetector" -> "3 [author]"
   "8 RenameAnalysis" -> "16 Burndown"
   "8 RenameAnalysis" -> "9 FileDiff"
   "8 RenameAnalysis" -> "10 UAST"
   "8 RenameAnalysis" -> "13 UASTChanges"
+  "1 TicksSinceStart" -> "4 [tick]"
   "2 TreeDiff" -> "5 [changes]"
   "10 UAST" -> "12 [uasts]"
   "13 UASTChanges" -> "14 [changed_uasts]"
-  "4 [author]" -> "16 Burndown"
+  "3 [author]" -> "16 Burndown"
   "7 [blob_cache]" -> "16 Burndown"
   "7 [blob_cache]" -> "9 FileDiff"
   "7 [blob_cache]" -> "8 RenameAnalysis"
@@ -47,8 +47,8 @@ func TestPipelineSerialize(t *testing.T) {
   "14 [changed_uasts]" -> "15 FileDiffRefiner"
   "5 [changes]" -> "6 BlobCache"
   "5 [changes]" -> "8 RenameAnalysis"
-  "3 [tick]" -> "16 Burndown"
   "11 [file_diff]" -> "15 FileDiffRefiner"
+  "4 [tick]" -> "16 Burndown"
   "12 [uasts]" -> "13 UASTChanges"
 }`, dot)
 }
@@ -68,20 +68,20 @@ func TestPipelineSerializeNoUast(t *testing.T) {
 	dot := string(bdot)
 	assert.Equal(t, `digraph Hercules {
   "6 BlobCache" -> "7 [blob_cache]"
-  "0 TicksSinceStart" -> "3 [tick]"
   "9 FileDiff" -> "10 [file_diff]"
-  "1 IdentityDetector" -> "4 [author]"
+  "0 IdentityDetector" -> "3 [author]"
   "8 RenameAnalysis" -> "11 Burndown"
   "8 RenameAnalysis" -> "9 FileDiff"
+  "1 TicksSinceStart" -> "4 [tick]"
   "2 TreeDiff" -> "5 [changes]"
-  "4 [author]" -> "11 Burndown"
+  "3 [author]" -> "11 Burndown"
   "7 [blob_cache]" -> "11 Burndown"
   "7 [blob_cache]" -> "9 FileDiff"
   "7 [blob_cache]" -> "8 RenameAnalysis"
   "5 [changes]" -> "6 BlobCache"
   "5 [changes]" -> "8 RenameAnalysis"
-  "3 [tick]" -> "11 Burndown"
   "10 [file_diff]" -> "11 Burndown"
+  "4 [tick]" -> "11 Burndown"
 }`, dot)
 }
 
