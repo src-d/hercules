de522fabb931b212f9f007f5b8945126931d3a99
Vadim Markovtsev <vadim@sourced.tech>
2019-09-28 20:40:39 +0200

    v10.4.3
    Signed-off-by: Vadim Markovtsev <vadim@sourced.tech>

--- python/setup.py
+++ python/setup.py
@@ -15,7 +15,7 @@ setup(
     description="Python companion for github.com/src-d/hercules to visualize the results.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version="10.4.2",
+    version="10.4.3",
     license="Apache-2.0",
     author="source{d}",
     author_email="machine-learning@sourced.tech",
